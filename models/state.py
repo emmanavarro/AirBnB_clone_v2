c649a8496bc0101123fbd167cb46e56aff7d5e7c
Emma Navarro <elnavarro55@gmail.com>
2020-08-11 15:42:46 +0000

    Update class State * State inherits from BaseModel and Base * Add class attribute __tablename__ * Update class attribute name * Add DBStorage and FileStorage

--- models/state.py
+++ models/state.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.city import City
+import models.engine.file_storage
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, ForeignKey
+from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False))
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref='state', cascade="all, delete")
+    else:
+        @property
+        def cities(self):
+            """Returns cities"""
+            all_cities = storage.all(City)
+            list_cities = []
+            for key, value in all_cities.items():
+                if self.id == value.state_id:
+                    list_cities.append(value)
+            return list_cities
