ebc69606ce592f420e6e5c7dd429adcf3d228cc8
Emma Navarro <elnavarro55@gmail.com>
2020-08-11 03:32:24 +0000

    Create a delete method to FileStorage and update all method * Added a new public instance method: def delete(self, obj=None): to delete obj from __objects if its inside * Updated the prototype of def all(self) to def all(self, cls=None) - that returns the list of objects of one type of class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,15 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls:
+            temp = {}
+            for key, value in self.__objects.items():
+                if key.split('.')[0] == cls.__name__:
+                    temp[key] = value
+            return temp
+        return self.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -48,3 +54,10 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Deletes an object if it's inside"""
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            if key in self.__objects:
+                del self.all()[key]
