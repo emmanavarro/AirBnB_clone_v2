f6491431ba906fe1845633fff4cae38d26deeebd
Emma Navarro <elnavarro55@gmail.com>
2020-08-12 21:07:35 +0000

    Change organization of the class attributes

--- models/state.py
+++ models/state.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from models.city import City
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String, ForeignKey
 from os import getenv
 
@@ -10,20 +9,21 @@ from os import getenv
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
 
-    # if getenv('HBNB_TYPE_STORAGE') == 'db':
-    cities = relationship('City', backref='state', cascade="all, delete")
-    # else:
+    name = Column(String(128),
+                  nullable=False)
 
-    @property
-    def cities(self):
-        """Returns cities"""
-        from models import storage
+    cities = relationship("City",
+                          backref='state',
+                          cascade="all, delete")
 
-        all_cities = storage.all(City)
-        list_cities = []
-        for key, value in all_cities.items():
-            if self.id == value.state_id:
-                list_cities.append(value)
-        return list_cities
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """Returns cities"""
+            all_cities = storage.all(City)
+            list_cities = []
+            for key, value in all_cities.items():
+                if self.id == value.state_id:
+                    list_cities.append(value)
+            return list_cities
