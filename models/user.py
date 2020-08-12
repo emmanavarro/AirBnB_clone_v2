42596c35a94ebbed581a80702f3ce1aee7aa93bd
Emma Navarro <elnavarro55@gmail.com>
2020-08-12 00:36:17 +0000

    Added class attribute palces

--- models/user.py
+++ models/user.py
@@ -2,6 +2,8 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer
+from sqlalchem.orm import relationship, backref
+from models.place import Place
 
 
 class User(BaseModel, Base):
@@ -12,3 +14,4 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+    places = relationship('Place', backref='user', cascade='all, delete')
