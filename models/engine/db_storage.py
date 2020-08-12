64ed5dfe0941ad464efba7e85fe4d69f46a98187
Emma Navarro <elnavarro55@gmail.com>
2020-08-12 21:00:57 +0000

    Change organization of create_engine

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -19,9 +19,11 @@ class DBStorage:
 
     def __init__(self):
         """ Create the engine """
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
-            getenv('HBNB_MYSQL_USER'), getenv('HBNB_MYSQL_PWD'),
-            getenv('HBNB_MYSQL_HOST'), getenv('HBNB_MYSQL_DB')),
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(getenv('HBNB_MYSQL_USER'),
+                                              getenv('HBNB_MYSQL_PWD'),
+                                              getenv('HBNB_MYSQL_HOST'),
+                                              getenv('HBNB_MYSQL_DB')),
                                       pool_pre_ping=True)
         if getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
