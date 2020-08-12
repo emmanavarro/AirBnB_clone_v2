4f1a54cab6d68384cbb199cc58a8a5629b816732
Emma Navarro <elnavarro55@gmail.com>
2020-08-12 20:56:54 +0000

    Change organization of class attributes id, created_at and updated_at

--- models/base_model.py
+++ models/base_model.py
@@ -12,9 +12,15 @@ Base = declarative_base()
 class BaseModel:
     """A base class for all hbnb models"""
 
-    id = Column(String(60), primary_key=True, nullable=False)
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    id = Column(String(60),
+                nullable=False,
+                primary_key=True)
+    created_at = Column(DateTime,
+                        nullable=False,
+                        default=datetime.utcnow())
+    updated_at = Column(DateTime,
+                        nullable=False,
+                        default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
@@ -23,8 +29,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            # self.save()
-
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -51,13 +55,10 @@ class BaseModel:
         dictionary.update(self.__dict__)
         dictionary.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
-
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-
         if '_sa_instance_state' in dictionary:  # deletes key if exists
             dictionary.pop('_sa_instance_state')
-
         return dictionary
 
     def delete(self):
