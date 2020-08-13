b22b94cf966c224a3b87c22a30364ede79327ab1
Emma Navarro <elnavarro55@gmail.com>
2020-08-13 04:50:38 +0000

    Return to previous changes

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,13 @@
 """ This module instantiates an object of
     class FileStorage and DBStorage """
 from os import getenv
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 if getenv('HBNB_TYPE_STORAGE') == 'db':
