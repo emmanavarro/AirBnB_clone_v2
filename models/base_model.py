d9c3f1f243a87afc75d3e841a231b9ee593c0725
Emma Navarro <elnavarro55@gmail.com>
2020-08-13 07:06:52 +0000

    Update the __init__ constructor

--- models/base_model.py
+++ models/base_model.py
@@ -30,23 +30,25 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            kwargs['created_at'] = datetime.strptime(
-                kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['updated_at'] = datetime.strptime(
-                kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
-            self.created_at =
-            datetime.now() if 'created_at' not in kwargs else pass
-            self.updated_at =
-            datetime.now() if 'updated_at' not in kwargs else pass
-            self.id = str(uuid.uuid4()) if 'id' not in kwargs else pass
-
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            for key, value in kwargs.items():
+                if key in ['created_at', 'updated_at']:
+                    setattr(self, key,
+                            datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                elif key not in ['__class__']:
+                    setattr(self, key, value)
+            if 'created_at' not in kwargs:
+                self.created_at = datetime.now()
+            if 'updated_at' not in kwargs:
+                self.updated_at = datetime.now()
+            if 'id' not in kwargs:
+                setattr(self, 'id', str(uuid.uuid4()))
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        dict = self.to_dict()
+        del dict['__class__']
+        return '[{}] ({}) {}'.format(
+                type(self).__name__, self.id, dict)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
@@ -63,7 +65,7 @@ class BaseModel:
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         if '_sa_instance_state' in dictionary:  # deletes key if exists
-            dictionary.pop('_sa_instance_state')
+            del dictionary['_sa_instance_state']
         return dictionary
 
     def delete(self):
