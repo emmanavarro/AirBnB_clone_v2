325bd26ea76b0728957235a685740f34a986ff2d
Emma Navarro <elnavarro55@gmail.com>
2020-08-13 05:50:52 +0000

    Updated conditionals in the constructor method

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
+import models
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, DateTime, String
@@ -25,15 +26,20 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
         if not kwargs:
-            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
             kwargs['created_at'] = datetime.strptime(
-                    kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+                kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
             kwargs['updated_at'] = datetime.strptime(
-                    kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+                kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            self.created_at =
+            datetime.now() if 'created_at' not in kwargs else pass
+            self.updated_at =
+            datetime.now() if 'updated_at' not in kwargs else pass
+            self.id = str(uuid.uuid4()) if 'id' not in kwargs else pass
+
             del kwargs['__class__']
             self.__dict__.update(kwargs)
 
@@ -44,10 +50,9 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
         self.updated_at = datetime.now()
-        storage.new(self)  # moved from __init__
-        storage.save()
+        models.storage.new(self)  # moved from __init__
+        models.storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
