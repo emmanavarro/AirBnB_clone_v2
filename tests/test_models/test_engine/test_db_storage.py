0674ed450217a754445e51fe93c5557c76d57707
crispthoalex <alex877@gmail.com>
2020-08-14 00:27:05 +0000

    Apply skipif to class methods

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -13,10 +13,11 @@ from models.engine import db_storage
 from models.engine.db_storage import DBStorage
 
 
-@unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db', 'environment = db')
+
 class Test_DB_Storage(unittest.TestCase):
     """ Class to test the DB storage method """
 
+    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db', 'environment = db')
     def test_pep8(self):
         """
         * Test pep8 style validation
@@ -29,14 +30,17 @@ class Test_DB_Storage(unittest.TestCase):
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warning).")
 
+    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db', 'environment = db')
     def test_module_doc(self):
         """ check for module documentation """
         self.assertTrue(len(db_storage.__doc__) > 0)
 
+    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db', 'environment = db')
     def test_class_doc(self):
         """ check for documentation """
         self.assertTrue(len(DBStorage.__doc__) > 0)
 
+    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db', 'environment = db')
     def test_method_docs(self):
         """ check for method documentation """
         for func in dir(DBStorage):
