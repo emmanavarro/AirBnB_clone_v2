b82659965b94a54ac739b62ca7eb0508ea5ffacd
emmanavarro <elnavarro55@gmail.com>
2020-09-01 16:57:00 -0500

    Add import of render_template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -4,7 +4,7 @@
     - Routes:
         - /: display "Hello HBNB!"
 """
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
