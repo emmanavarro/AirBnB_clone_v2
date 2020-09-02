754ae138af0825dea0fccea88946deca98ce0019
emmanavarro <elnavarro55@gmail.com>
2020-09-02 00:23:46 -0500

    Add methods

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -9,6 +9,20 @@ from models.state import State
 app = Flask(__name__)
 
 
+@app.route('/states_list', strict_slashes=False)
+def list_states():
+    """ List all the states """
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def list_cities_states():
+    """ List all the cities in a state """
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states)
+
+
 @app.route('/states', strict_slashes=False)
 def states():
     """ List all the states """
