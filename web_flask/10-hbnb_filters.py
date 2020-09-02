2eab686067ad0fb19f48ca7da6c90cd7e9be4a43
emmanavarro <elnavarro55@gmail.com>
2020-09-02 02:23:04 -0500

    Method to list states and citites dynamically

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+""" Script that starts a Flask web application """
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.amenity import Amenity
+
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def list_states():
+    """ List all the states """
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def list_cities_states():
+    """ List all the cities in a state """
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.route('/states', strict_slashes=False)
+def states():
+    """ List all the states """
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def states_id(id):
+    """ List all the states by id """
+    flag = 0
+    states = None
+    all_states = storage.all(State).values()
+    for state in all_states:
+        if id in state.id:
+            flag = 1
+            states = state
+            break
+    return render_template('9-states.html', states=states, flag=flag)
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
+    """ List the states and its cities dynamically """
+    states = storage.all(State).values()
+    amenities = storage.all(Amenity).values()
+    return render_template('10-hbnb_filters.html',
+                           states=states, amenities=amenities)
+
+
+@app.teardown_appcontext
+def close_db(db):
+    """ Remove the current SQLAlchemy Session """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
