32d4d956ebcd3652b754354239edc9b9850e93de
emmanavarro <elnavarro55@gmail.com>
2020-09-02 00:11:57 -0500

    Method to list all states by id

---
+++ web_flask/9-states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Script that starts a Flask web application """
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
+def states_id(id=None):
+    """ List all the states by id """
+    flag = 0
+    states = None
+    all_states = storage.all(State).values()
+    for state in all_states:
+        if id in state.id:
+            flag = 1
+            states = state
+            break
+    return render_template('9-states.py', states=states, flag=flag)
+
+
+@app.teardown_appcontext
+def close_db(db):
+    """ Remove the current SQLAlchemy Session """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
